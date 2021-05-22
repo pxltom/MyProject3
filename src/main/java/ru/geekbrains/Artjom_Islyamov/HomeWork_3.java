ed0bb9c6b6522e0815fff67e55f1c35f24a7733a
Artjom Islyamov <pixeltom@yandex.ru>
2021-05-22 15:06:31 +0300

    2 tasks corrected

--- src/main/java/ru/geekbrains/Artjom_Islyamov/HomeWork_3.java
+++ src/main/java/ru/geekbrains/Artjom_Islyamov/HomeWork_3.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 public class HomeWork_3 {
     public static void main(String[] args) {
         int[] a = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        int b = a.length;
-        for (int i = 0; i < b; i++) {
+         for (int i = 0; i < a.length; i++) {
             if (a[i] == 1) {
                 a[i] = 0;
             } else a[i] = 1;
         }
         System.out.println(Arrays.toString(a));
 
-        int [] c = new int[100];
-        for (int i = 0; i < 100 ; i++) {
-            c [i]= i + 1;
+        int[] c = new int[100];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = i + 1;
             System.out.println("b [" + i + "] = " + c[i]);
 //
 //
 //        }
+        }
     }
-}}
+}
